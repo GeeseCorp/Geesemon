f67b78ce0a7b030ad56a1cf4c256c93ef0e18997
Vitalii Vaskivskyi <ipz204_vvyu@student.ztu.edu.ua>
2022-09-15 20:31:33 +0300

    updated huge todo

--- Geesemon.DataAccess/Providers/UsersChatsProvider/UserChatProvider.cs
+++ Geesemon.DataAccess/Providers/UsersChatsProvider/UserChatProvider.cs
@@ -25,7 +25,7 @@ namespace Geesemon.DataAccess.Providers.UsersChatsProvider
             return userChat;
         }
 
-        // TODO: huge lox must fix this method
+        // TODO: huge lox must fix this method!!!
         public async Task<List<UserChat>> GetPersonalByUserIds(params Guid[] userIds)
         {    
             var chat = await context.Chats.Include(uc => uc.UserChats)
