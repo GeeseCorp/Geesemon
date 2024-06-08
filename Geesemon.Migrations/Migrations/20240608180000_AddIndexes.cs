6c1a9799ad23a09840dbdeec75252aa1146c7529
Vitalii Vaskivskyi <v.vaskivskyi@sana-commerce.com>
2024-06-08 19:31:41 +0300

    add indexes

---
+++ Geesemon.Migrations/Migrations/20240608180000_AddIndexes.cs
@@ -0,0 +1,21 @@
+﻿using FluentMigrator;
+
+namespace Geesemon.Migrations.Migrations;
+
+[Migration(20240608180000)]
+public class _20240608180000_AddIndexes : Migration
+{
+    public override void Up()
+    {
+        Execute.Sql("ALTER TABLE Users ALTER COLUMN FirstName nvarchar(100) NOT NULL");
+        Execute.Sql("ALTER TABLE Users ALTER COLUMN LastName nvarchar(100) NOT NULL");
+        Execute.Sql("ALTER TABLE Users ALTER COLUMN Email nvarchar(100) NOT NULL");
+        Execute.Sql("ALTER TABLE Users ALTER COLUMN Identifier nvarchar(100) NOT NULL");
+        Execute.Sql("ALTER TABLE Chats ALTER COLUMN Identifier nvarchar(100) NULL");
+
+        Execute.Sql("CREATE UNIQUE NONCLUSTERED INDEX Identifier_Users ON Users (Identifier)");
+        Execute.Sql("CREATE UNIQUE NONCLUSTERED INDEX Email_Users ON Users (Email)");
+        Execute.Sql("CREATE NONCLUSTERED INDEX FirstName_LastName_Users ON Users (FirstName, LastName)");
+        Execute.Sql("CREATE NONCLUSTERED INDEX Identifier_Chats ON Chats (Identifier)");
+    }
+}
